6b38896d40fbfaae1538ba59cd2a9d48bcd7cf0c
TejeVarma <ankani.teja@gmail.com>
2023-10-21 20:08:38 +0530

    Created vowel program

--- Vowels_program.py
+++ Vowels_program.py
@@ -1,2 +1,12 @@
+#Check if the number is even or odd
+#A number is  even  if dividible by 2 gives a reminder is zero
+#If the remainder is 1, it is an Odd Number
+num=int(input("Enter the Number:"))
+#if (num<=0):
+   #print("Number is invalid:")
+if num%2==0:
+    print("{} is Even Number")
+else:
+    print("{} is odd Number")
 
 
